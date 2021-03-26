3d4ef375238ef0680466eb3d25b69b064a3a88e2
Daniel Baesso <danielbaesso@gmail.com>
2021-03-26 18:30:24 -0300

    Original script

---
+++ birthday_pi.py
@@ -0,0 +1,15 @@
+filename = "https://github.com/ehmatthes/pcc/blob/master/chapter_10/pi_million_digits.txt"
+
+with open(filename) as file_object:
+    lines = file_object.readlines()
+
+    pi_string = ""
+
+    for line in lines:
+        pi_string += line.strip()
+
+    b_day = input("Enter your birthday in the form mmddaa: ")
+    if b_day in pi_string:
+        print("Your birthday appears in the first million digits of pi!")
+    else:
+        print("Your birthday does not appears in the first million digits of pi.")
